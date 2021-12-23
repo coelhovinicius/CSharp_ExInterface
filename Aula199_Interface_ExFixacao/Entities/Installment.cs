1a85c66d594cd0958620dedf8dfc9c0306559662
coelhovinicius <coelhovinicius@yahoo.com.br>
2021-12-23 20:34:20 -0300

    Exercicio de Interfaces

---
+++ Aula199_Interface_ExFixacao/Entities/Installment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula199_Interface_ExFixacao.Entities
+{
+    class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment()
+        {
+        }
+
+        public Installment(DateTime date, double amount)
+        {
+            DueDate = date;
+            Amount = amount;
+        }
+
+        // Override
+        public override string ToString()
+        {
+            return DueDate.ToString("dd/MM/yyyy") + " - " + Amount.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Aula199_Interface_ExFixacao/Services/IOnlinePaymentService.cs
@@ -0,0 +1,8 @@
+﻿namespace Aula199_Interface_ExFixacao.Services
+{
+    interface IOnlinePaymentService
+    {
+        double PaymentFee(double amount);
+        double Interest(double amount, int months);
+    }
+}
